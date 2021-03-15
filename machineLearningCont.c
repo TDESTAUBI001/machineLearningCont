61f84e7e4d8eda3600c8b923c5a911f7ace2890d
TDESTAUBI001 <61202422+TDESTAUBI001@users.noreply.github.com>
2021-03-15 16:33:48 -0500

    Update machineLearningCont.c

--- machineLearningCont.c
+++ machineLearningCont.c
@@ -1,6 +1,6 @@
  
 /*
-Copyright (c) 2016-2020 Jeremy Iverson
+Copyright (c) 2016-2020 Thomas de St. Aubin and Jeremy Iverson
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
